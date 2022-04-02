d079fca39663e05efa7a229f4183627aab23c574
jiaxinyu <17635911140@163.com>
2022-04-02 15:05:48 +0800

    外网清船

---
+++ Artifact/Ship/clearShip.py
@@ -0,0 +1,27 @@
+import requests
+
+
+#一键清船
+def clear(user_id,ship_id):
+    url = "https://ranch.ifreetalk.com/testToolsController/clearShipSail/user_id/"+str(user_id)+"/ship_id/"+str(ship_id)
+    print(url)
+    rest = requests.get(url, stream=True)
+    s = rest.raw.read()
+    # print(s.decode('utf-8'))
+
+
+
+
+def use(user_id_list):
+    for ship_id in range(1,6):
+        clear(user_id_list,ship_id)
+
+
+
+if __name__ == "__main__":
+    user_id_list = [
+        
+    ]
+    for user_id in user_id_list:
+        use(user_id)
+
