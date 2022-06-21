84959c8305fc08144b37d1cd1482bd38b64d5811
jiaxinyu <17635911140@163.com>
2022-06-22 01:08:32 +0800

    内网清船

--- Ship/内clearShip.py
+++ Ship/内clearShip.py
@@ -1,3 +1,23 @@
-
-# http://10.101.70.8:8080/user/ship/clear_user_ship.html?user=9003200100011887
 # 内网清船
+import requests
+
+
+#一键清船
+def clear(user_id):
+    url = "http://10.101.70.8:8080/user/ship/clear_user_ship.html?user="+str(user_id)
+    print(url)
+    rest = requests.get(url, stream=True)
+    print(rest.text)
+    # s = rest.raw.read()
+    # print(s.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    # 用户列表
+    user_id_list = [
+9003200100012310,
+9003200100012311,
+9003200100012565
+    ]
+    for user_id in user_id_list:
+        clear(user_id)
